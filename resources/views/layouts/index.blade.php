9cfdceb4f42bda6c12d06f83a21d399a1c02fe3e
Marioji <jimenezrubiom@gmail.com>
2021-04-14 09:41:00 +0200

    Primera version

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+</head>
+<body>
+    <p>Vale, en principio esto es solo front, your turn :)</p>
+</body>
+</html>
